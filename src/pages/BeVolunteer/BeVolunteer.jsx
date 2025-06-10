ce98bdb7dea33d6e76deae8d3a6a7bf73e0e5730
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-06-10 14:38:50 +0600

    be a volunteer modal created with required form fields

---
+++ src/pages/BeVolunteer/BeVolunteer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BeVolunteer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BeVolunteer;
--- src/pages/VolunteerNeedPostDetails/VolunteerNeedPostDetails.jsx
+++ src/pages/VolunteerNeedPostDetails/VolunteerNeedPostDetails.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import DatePicker from 'react-datepicker';
 import { useLoaderData} from 'react-router';
+import useAuth from '../../hooks/useAuth';
 
 const VolunteerNeedPostDetails = () => {
-    const {_id,title,thumbnail,description,deadline,category,location,noOfVolunteers} = useLoaderData();
+    const {_id,title,thumbnail,description,deadline,category,location,noOfVolunteers,oname,oemail} = useLoaderData();
+    const {user} = useAuth();
+
+    const handleVolunteer = e =>{
+        e.preventDefault();
+        console.log("Your application has been submitted")
+    }
     
     return (
         <div className='max-w-7xl mx-auto my-20'>
@@ -21,7 +29,116 @@ const VolunteerNeedPostDetails = () => {
                 <p className='text-base font-normal'>No. of Volunteers: {noOfVolunteers}</p>
                 
                 <div className="card-actions">
-                <button className="btn btn-primary">Be a Volunteer</button>
+                    <button onClick={()=>document.getElementById('my_modal_5').showModal()} className="btn btn-primary">Be a Volunteer</button>
+                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-2xl text-center">Be a Volunteer!</h3>
+                        <div className="modal-action">
+                        <form onSubmit={handleVolunteer} className='fieldset'>
+                            <legend className="fieldset-legend">Post details</legend>
+                            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Thumbnail</label>
+                                    <input type="text" name='thumbnail'
+                                    defaultValue={thumbnail} className="input w-full" placeholder="Thumbnail" readOnly />
+                                </fieldset>
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Post Title</label>
+                                    <input type="text" name='title'
+                                    defaultValue={title} className="input w-full" placeholder="Post Title" readOnly />
+                                </fieldset>
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Description</label>
+                                    <textarea name='description'
+                                    defaultValue={description} rows="3" className="textarea w-full" placeholder="Description"
+                                    readOnly></textarea>
+                                </fieldset>
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Category</label>
+                                    <select
+                                    value={category}
+                                    className="select w-full bg-gray-100 cursor-not-allowed"
+                                    disabled>
+                                        <option disabled={true}>Select a category</option>
+                                        <option value="healthcare">Health Care</option>
+                                        <option value="education">Education</option>
+                                        <option value="social service">Social Service</option>
+                                        <option value="animal welfare">Animal Welfare</option>
+                                    </select>
+                                </fieldset>
+                                                        
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Location</label>
+                                    <input type="text" name='location' 
+                                    defaultValue={location} className="input w-full" placeholder="Location" 
+                                    readOnly/>
+                                </fieldset>
+                                
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">No. of volunteers needed</label>
+                                    <input type="number" name='noOfVolunteers'
+                                    defaultValue={noOfVolunteers} className="input w-full" placeholder="No. of volunteers" 
+                                    readOnly/>
+                                </fieldset>
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Deadline</label>
+                                    <DatePicker
+                                    value={deadline}
+                                    name='deadline'
+                                    className='input w-full'
+                                    readOnly
+                                    />
+                                </fieldset>
+                            
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Organizer Name</label>
+                                    <input type="text" name='oname' defaultValue={oname} className="input w-full" placeholder="User Name" readOnly/>
+                                </fieldset>
+                            
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Organizer Email</label>
+                                    <input type="email" name='oemail' 
+                                    defaultValue={oemail} className="input w-full" placeholder="Email" readOnly />
+                                </fieldset>
+                            </div>
+                            <legend className="fieldset-legend">Volunteer details</legend>
+                            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Volunteer Name</label>
+                                    <input type="text" name='vname' defaultValue={user?.displayName} className="input w-full" placeholder="Volunteer Name" readOnly/>
+                                </fieldset>
+                            
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Volunteer Email</label>
+                                    <input type="email" name='oemail' 
+                                    defaultValue={user?.email} className="input w-full" placeholder="Volunteer Email" readOnly />
+                                </fieldset>
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Suggestion</label>
+                                    <textarea name='suggestion'
+                                    rows="3" className="textarea w-full" placeholder="Suggestion"
+                                    ></textarea>
+                                </fieldset>
+
+                                <fieldset className='fieldset rounded-box p-4'>
+                                    <label className="label">Status</label>
+                                    <select name="status" defaultValue="requested" className="select w-full">
+                                    <option value="requested">Requested</option>
+                                    <option value="approved">Approved</option>
+                                    <option value="rejected">Rejected</option>
+                                    </select>
+
+                                </fieldset>
+
+                            </div>
+                            <div className='p-4'>
+                                <input type='submit' className='btn w-full btn-primary' value='Request'/>
+                            </div>
+                            <button type='button' className="btn" onClick={() => document.getElementById('my_modal_5').close()}>Close</button>
+                        </form>
+                        </div>
+                    </div>
+                    </dialog>
                 </div>
             </div>
             </div>
