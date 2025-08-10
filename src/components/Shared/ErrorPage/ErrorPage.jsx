c8506d9a1333be6055243b5620263ccd488fa34f
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-08-10 14:38:31 +0600

    error page go back icon added and button color changed, deployed to firebase

--- src/components/Shared/ErrorPage/ErrorPage.jsx
+++ src/components/Shared/ErrorPage/ErrorPage.jsx
@@ -3,6 +3,7 @@ import errorImg from '../../../assets/404-page-n3.png'
 import Navbar from '../../Navbar/Navbar';
 import Footer from '../../Footer/Footer';
 import { Link } from 'react-router';
+import { FaArrowLeftLong } from 'react-icons/fa6';
 const ErrorPage = () => {
     return (
         <>
@@ -12,7 +13,10 @@ const ErrorPage = () => {
             <div className='flex flex-col items-center justify-center gap-3 min-h-screen my-10'>
             <img className='h-[350px] object-cover' src={errorImg} alt="" />
             <Link to='/'>
-                <button className='btn btn-primary'>Go Back Home</button>
+                <button className='btn flex bg-[#2dcfc4] text-white rounded-xl'>
+                    <span><FaArrowLeftLong /></span>
+                    <span>Go Back Home</span>
+                </button>
             </Link>
             </div>
         </div>
